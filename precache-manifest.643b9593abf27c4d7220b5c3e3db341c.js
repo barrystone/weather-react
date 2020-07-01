089d8bfdfd1d9a820f399ec3f534a96522aab6d4
barrystone <barrystone3183@gmail.com>
2020-07-01 16:11:35 +0800

    Updates

---
+++ precache-manifest.643b9593abf27c4d7220b5c3e3db341c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8828721ca3b9ffeb75f1595af33c8fb",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "5a532bce01e6cd7de672",
+    "url": "/weather-react/static/css/main.206fe0e0.chunk.css"
+  },
+  {
+    "revision": "9f8f46f507a564286c40",
+    "url": "/weather-react/static/js/2.aaabdeb5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather-react/static/js/2.aaabdeb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a532bce01e6cd7de672",
+    "url": "/weather-react/static/js/main.24a525b0.chunk.js"
+  },
+  {
+    "revision": "b3ac8c737164b9cadc83",
+    "url": "/weather-react/static/js/runtime-main.a59de9ef.js"
+  },
+  {
+    "revision": "d75a0472ffa15b3b6dbed6cc09ae2352",
+    "url": "/weather-react/static/media/bg1.d75a0472.jpg"
+  },
+  {
+    "revision": "0e773da2511505d5a9ca70d492bdf4df",
+    "url": "/weather-react/static/media/bg2.0e773da2.jpg"
+  }
+]);
